4ec157cb9042fa79e6250819b3b3c0580d927514
jlaurice <55763317+jlaurice@users.noreply.github.com>
2020-08-17 17:04:36 -0400

    Added new task modal dialog to TodoListWithVuex example

--- src/components/Tasks/Task.vue
+++ src/components/Tasks/Task.vue
@@ -53,7 +53,8 @@ export default {
     ...mapActions('tasks', ['updateTask', 'deleteTask']),
 
     promptToDelete(id) {
-      //Here we use the Dialog (a Quasar Plugin) to ask the user if they really want to delete
+      //Here we use the Dialog plugin to ask the user if they really want to delete ...
+      //Note:  There is also a Dialog (as a Quasar Component) which we also use in this example when displaying the add tasks form
       this.$q
         .dialog({
           title: 'Confirm',
--- src/pages/examples/TodoListWithVuex.vue
+++ src/pages/examples/TodoListWithVuex.vue
@@ -5,42 +5,18 @@
   <q-page class="q-pa-md">
     <p>Todo Page</p>
     <q-list separator bordered>
-      <q-item
-        v-for="task in tasks"
-        :key="task.id"
-        @click="task.completed = !task.completed"
-        :class="!task.completed ? 'bg-orange-1' : 'bg-green-1'"
-        v-ripple
-      >
-        <q-item-section side top>
-          <q-checkbox v-model="task.completed"/>
-        </q-item-section>
-
-        <q-item-section>
-          <!-- Here we conditionally add the specified class only when the task is completed -->
-          <!-- Because this is our own style class that we might want to use throughout the app a good place to define it is in the root vue component which is App.vue-->
-          <q-item-label :class="{ 'text-strikethrough' : task.completed }">{{ task.name }}</q-item-label>
-          <!-- <q-item-label caption>Notify me about updates to apps or games that I downloaded</q-item-label> -->
-        </q-item-section>
-
-        <q-item-section side>
-          <!-- Here we will lay out these elements into a row and then use a column to stack them together - See Quasar Docs for flexbox documentation for usage examples -->
-          <div class="row">
-            <div class="column justify-center">
-              <!-- Here we add a class to give space between the calendar and the date.  The "mr" means margin right and "xs" means extra small -->
-              <q-icon name="event" size="18px" class="q-mr-xs"/>
-            </div>
-            <div class="column">
-              <!-- Here we add a class to move the date and time all the way to the right (aka. end) -->
-              <q-item-label class="row justify-end" caption>{{ task.dueDate }}</q-item-label>
-              <q-item-label class="row justify-end" caption>
-                <small>{{ task.dueTime }}</small>
-              </q-item-label>
-            </div>
-          </div>
-        </q-item-section>
-      </q-item>
+      <task v-for="(task, key) in tasks" :key="key" :task="task" :id="key"></task>
     </q-list>
+
+    <!-- View Quasar Docs (Style&Identity > Positioning) for a style such as absolute-bottom for positioning the button at screen bottom-->
+    <!-- We also add the q-mb-lg style class to give some space at the bottom so the button is not at the very bottom ("m" for margin, "b" for bottom, and "lg" for large)-->
+    <div class="absolute-bottom text-center q-mb-lg">
+      <q-btn @click="showAddTask = true" round color="primary" size="24px" icon="add"/>
+    </div>
+
+    <q-dialog v-model="showAddTask">
+      <add-task />
+    </q-dialog>
   </q-page>
 </template>
 
@@ -48,6 +24,15 @@
 <script>
 import { mapGetters } from 'vuex'
 export default {
+  components: {
+    task: require('components/Tasks/Task.vue').default,
+    'add-task': require('components/Tasks/Modals/AddTask.vue').default
+  },
+  data() {
+    return {
+      showAddTask: false
+    }
+  },
   computed: {
     //This demonstrates one of few ways to get data from the store ... the other way is to use the mapState or mapGetters helper functions
     // tasks() {
