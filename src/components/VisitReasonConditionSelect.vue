4c463b33567e6725262a1c3e27be7598caef3aa5
jlaurice <55763317+jlaurice@users.noreply.github.com>
2020-08-18 17:42:43 -0400

    Added visit reason and resaon condition select lists

---
+++ src/components/VisitReasonConditionSelect.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="q-ma-md">Condition:
+    <q-select
+      filled
+      v-model="model"
+      emit-value
+      map-options
+      input-debounce="0"
+      option-value="id"
+      option-label="name"
+      label="Select Condition"
+      :options="options"
+      @input="emitSelectChangeEvt"
+      style="width: 250px;"
+    ></q-select>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      model: '',
+      options: [
+        {
+          id: 1,
+          name: 'Current Ear Infection'
+        },
+        {
+          id: 2,
+          name: 'Discharge'
+        },
+        {
+          id: 3,
+          name: 'Dizziness'
+        },
+        {
+          id: 4,
+          name: 'Fullness or Clogging'
+        },
+        {
+          id: 5,
+          name: 'Hearing Loss, Sudden'
+        }
+      ]
+    }
+  },
+  methods: {
+    emitSelectChangeEvt(val) {
+      const conditionId = val + ''
+      if (conditionId.length > 0) {
+        console.log(
+          'Visit Reason Condition selection change was made, conditionId is : ' +
+            conditionId
+        )
+        this.$emit('change', conditionId)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
