2d196a6e94c2bcab79050c50c530b81a37ddfd8b
jlaurice <55763317+jlaurice@users.noreply.github.com>
2020-08-19 20:13:54 -0400

    Added third step content

---
+++ src/components/StepContentPatientInfo.vue
@@ -0,0 +1,66 @@
+<template>
+  <q-card>
+    <q-card-section>
+      <div class="q-pa-lg">Are you a new patient to XYZ ?
+        <q-option-group v-model="newPatientFlg" :options="options" color="primary" inline/>
+      </div>
+
+      <div class="q-pa-md" style="max-width: 400px">Is there a specific date that you would prefer?*
+        <q-input
+          outlined
+          clearable
+          label="Preferred date"
+          v-model="prefDate"
+          @clear="$emit('clear')"
+        >
+          <template v-slot:append>
+            <q-icon name="event" class="cursor-pointer">
+              <q-popup-proxy ref="qDateProxy" transition-show="scale" transition-hide="scale">
+                <q-date v-model="prefDate"/>
+              </q-popup-proxy>
+            </q-icon>
+          </template>
+        </q-input>
+      </div>
+
+      <!-- Choose Preferred Time -->
+      <div class="q-pa-md" style="max-width: 350px">Is there a specific time that you would prefer?*
+        <q-input outlined clearable label="Preferred time" v-model="prefTime" class="col">
+          <template v-slot:append>
+            <q-icon name="access_time" class="cursor-pointer">
+              <q-popup-proxy transition-show="scale" transition-hide="scale">
+                <q-time v-model="dueTime"/>
+              </q-popup-proxy>
+            </q-icon>
+          </template>
+        </q-input>
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newPatientFlg: '',
+      prefDate: '',
+      prefTime: '',
+
+      options: [
+        {
+          label: 'YES',
+          value: 'Y'
+        },
+        {
+          label: 'NO',
+          value: 'N'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
